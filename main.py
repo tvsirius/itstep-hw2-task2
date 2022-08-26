47fe6259ab1a06b80d81a46b818f9cfe352a8239
Denis Savitsky <densav@gmail.com>
2022-08-26 16:29:59 +0300

    weather api works

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import requests
 from openweather_api import openweather_api
+import pprint
+
+lat=30.489772
+lon=-99.771335
+
+exclude='minutely,hourly,daily,alerts'
+
+units='metric'
+
+weather_get=requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={exclude}&appid={openweather_api}&lang=ru&units={units}')
 
 
-lat=lon=0
-exclude=0
 
-weather=requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={openweather_api}')
+pprint.pprint(weather_get.json())
+
+
